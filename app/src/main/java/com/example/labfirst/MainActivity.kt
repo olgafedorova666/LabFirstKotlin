82afa192b4bc93e860c7f6ed964403161587a14e
Olga Fedorova <olgafedorova666@gmail.com>
2022-11-12 11:25:31 +0300

    Исправлен вызов функции window.SetFlags

--- app/src/main/java/com/example/labfirst/MainActivity.kt
+++ app/src/main/java/com/example/labfirst/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.labfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       window.setFtags(WindowsManager.LayoutParams.FLAG_SECURE, WindowsManager.LayoutParams.FLAG_SECURE)
+       window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
